9a8734edb50453793445996dd427b2e9f1adab82
fgiongo <fgiongo@protonmail.com>
2024-02-26 16:44:51 -0300

    bugfix

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from pySerialTransfer import pySerialTransfer as txfer
 import time
 
+global link
+link = txfer.SerialTransfer('/dev/ttyACM0', 9600)
 
 def handleBNO08x():
     accel = []
+    gyro = []
+    magnet = []
+    linaccel = []
+
     accel.append(link.rx_obj(obj_type='f', start_pos=0))
     accel.append(link.rx_obj(obj_type='f', start_pos=4))
     accel.append(link.rx_obj(obj_type='f', start_pos=8))
 
-    gyro = []
     gyro.append(link.rx_obj(obj_type='f', start_pos=12))
     gyro.append(link.rx_obj(obj_type='f', start_pos=16))
     gyro.append(link.rx_obj(obj_type='f', start_pos=20))
 
-    magnet = []
     magnet.append(link.rx_obj(obj_type='f', start_pos=24))
     magnet.append(link.rx_obj(obj_type='f', start_pos=28))
     magnet.append(link.rx_obj(obj_type='f', start_pos=32))
 
-    linaccel = []
     linaccel.append(link.rx_obj(obj_type='f', start_pos=36))
     linaccel.append(link.rx_obj(obj_type='f', start_pos=40))
     linaccel.append(link.rx_obj(obj_type='f', start_pos=44))
@@ -40,9 +43,12 @@ def handleBNO08x():
     for i in linaccel:
         print(i)
 
-    return accel, gyro, magnet, linaccel
 
 def handleBME688():
+    temp = 0
+    pressure = 0
+    humidity = 0
+
     temp = link.rx_obj(obj_type='f', start_pos=0)
     pressure = link.rx_obj(obj_type='f', start_pos=4)
     humidity = link.rx_obj(obj_type='f', start_pos=8)
@@ -55,16 +61,12 @@ def handleBME688():
     print('humidity:')
     print(humidity)
 
-    return temp, pressure, humidity
-
 
 callback_list = [ handleBNO08x, handleBME688 ]
 
 
 if __name__ == '__main__':
     try:
-
-        link = txfer.SerialTransfer('/dev/ttyACM0', 9600)
         link.set_callbacks(callback_list)
         link.open()
         time.sleep(2)
@@ -72,6 +74,6 @@ if __name__ == '__main__':
         while True:
             link.tick()
 
-    except KeyboadInterrupt:
+    except KeyboardInterrupt:
             link.close()
 
